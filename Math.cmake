94cfebb0959e42eaf79d28a46900967e1fd8fbb6
Emily Banerjee <prs.ttg+gh@pm.me>
2025-12-17 22:59:55 +0000

    math: add -mtune/march=native wrapper

--- Math.cmake
+++ Math.cmake
@@ -2,21 +2,36 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/Utilities.cmake)
 
-function(_c_flag_release_expr_conditional FLAG)
-    c_flag_test(FLAG _HAVE_FLAG)
-    if(${_HAVE_FLAG})
-        add_compile_options($<$<CONFIG:Release,RelWithDebInfo>:${FLAG}>)
-    endif()
+function(_c_flag_conditional_target TARGET FLAG)
+	c_flag_test(FLAG _HAVE_FLAG)
+	if(${_HAVE_FLAG})
+		string(APPEND ${TARGET} " ${FLAG}")
+	endif()
+endfunction()
+
+function(_global_c_flag_release_conditional FLAG)
+	_c_flag_conditional_target(CMAKE_C_FLAGS_RELEASE ${FLAG})
+	_c_flag_conditional_target(CMAKE_C_FLAGS_RELWITHDEBINFO ${FLAG})
+endfunction()
+
+function(_global_c_flag_conditional FLAG)
+	_c_flag_conditional_target(CMAKE_C_FLAGS ${FLAG})
 endfunction()
 
 ### Globally enables FastMath in release builds.
 function(enable_fast_math)
-    # Adapted from https://github.com/CelestiaProject/Celestia/blob/master/cmake/FastMath.cmake
-    # Which is licenced under the GNU GPLv2.
+	# Adapted from https://github.com/CelestiaProject/Celestia/blob/master/cmake/FastMath.cmake
+	# Which is licenced under the GNU GPLv2.
 
-    _c_flag_release_expr_conditional(/fp:fast)
-    _c_flag_release_expr_conditional(-ffast-math)
-    _c_flag_release_expr_conditional(-fno-finite-math-only)
-    _c_flag_release_expr_conditional(-fsigned-zeros)
-    _c_flag_release_expr_conditional(-fno-associative-math)
+	_global_c_flag_release_conditional(/fp:fast)
+	_global_c_flag_release_conditional(-ffast-math)
+	_global_c_flag_release_conditional(-fno-finite-math-only)
+	_global_c_flag_release_conditional(-fsigned-zeros)
+	_global_c_flag_release_conditional(-fno-associative-math)
+endfunction()
+
+### Globally enables host-native CPU tuning.
+function(enable_host_tuning)
+	_global_c_flag_conditional(-march=native)
+	_global_c_flag_conditional(-mtune=native)
 endfunction()
